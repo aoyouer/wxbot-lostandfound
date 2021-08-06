b2fe1de81910841a3b162c2b4bc45b0d73fe41fb
haojie06 <haojie06@qq.com>
2021-08-06 20:50:32 +0800

    避免conversation被覆盖

--- bot/conversation.go
+++ bot/conversation.go
@@ -97,21 +97,19 @@ func switchStage(ctx conversation.ConversationContext, targetStage int) (err err
 // 阶段0 设置类型
 // 当前不使用阶段嵌套,避免过多层的嵌套调用
 func stage0Conversation(ctx conversation.ConversationContext) (err error) {
-	conversation := conversationMap[ctx.ReceiveContent.FromUsername]
-	conversation.LastActive = time.Now()
 	switch ctx.ReceiveContent.Content {
 	case "1", "我丢失了物品", "丢失物品":
-		conversation.Stage = 1
-		conversation.Type = 1
+		ctx.Conversation.Stage = 1
+		ctx.Conversation.Type = 1
 		// 虽然可以直接调用 stage1,但是为了避免过多层的嵌套,还是只进行回复
 		err = replyTextWithCtx(ctx, askLostOperationPrompt)
 	case "2", "我捡到了物品", "捡到物品":
-		conversation.Stage = 1
-		conversation.Type = 2
+		ctx.Conversation.Stage = 1
+		ctx.Conversation.Type = 2
 		err = replyTextWithCtx(ctx, askFoundOperationPrompt)
 	case "3", "我是管理员":
 		// TODO 允许企业中指定身份的人进行一些管理操作
-		conversation.Type = 3
+		ctx.Conversation.Type = 3
 	default:
 		// 无效输入
 		err = replyTextWithCtx(ctx, generalInvalidPrompt)
@@ -121,20 +119,18 @@ func stage0Conversation(ctx conversation.ConversationContext) (err error) {
 
 // 阶段1 设置操作 添加或者是查看
 func stage1Conversation(ctx conversation.ConversationContext) (err error) {
-	conversation := conversationMap[ctx.ReceiveContent.FromUsername]
-	conversation.LastActive = time.Now()
 	switch ctx.ReceiveContent.Content {
 	case "1", "添加丢失物品的记录", "添加捡到物品的记录":
-		conversation.Operation = "add"
-		if conversation.Type == 1 {
-			conversation.Stage = 2
+		ctx.Conversation.Operation = "add"
+		if ctx.Conversation.Type == 1 {
+			ctx.Conversation.Stage = 2
 			err = replyTextWithCtx(ctx, askLostPlacePrompt)
 		} else {
-			conversation.Stage = 2
+			ctx.Conversation.Stage = 2
 			err = replyTextWithCtx(ctx, askFoundPlacePrompt)
 		}
 	case "2", "查看捡到的物品列表", "查看失物记录列表":
-		conversation.Operation = "list"
+		ctx.Conversation.Operation = "list"
 		// TODO 展示已经记录的列表
 	default:
 		err = replyTextWithCtx(ctx, generalInvalidPrompt)
@@ -144,26 +140,29 @@ func stage1Conversation(ctx conversation.ConversationContext) (err error) {
 
 // 阶段2 添加地点 需要确认
 func stage2AddConversation(ctx conversation.ConversationContext) (err error) {
-	// TODO 对城市进行检查
-	conversation := conversationMap[ctx.ReceiveContent.FromUsername]
 	content := ctx.ReceiveContent.Content
 	switch ctx.Conversation.Status {
 	case "":
 		if !utils.IfWordInSlice(content, utils.CitySlice) {
 			err = replyTextWithCtx(ctx, cityInvalidPrompt)
 		} else {
-			conversation.Status = "waitconfirm"
-			conversation.Form.City = content
+			ctx.Conversation.Status = "waitconfirm"
+			ctx.Conversation.Form.City = content
 			// 询问,要求确认城市名称无误
 			err = replyTextWithCtx(ctx, fmt.Sprintf("您所在的城市是:%s\n1.yes\n2.no", content))
 		}
 	case "waitconfirm":
 		// 要求进行确认
-		conversation.Status = ""
+		ctx.Conversation.Status = ""
 		switch content {
 		case "1", "yes":
-			conversation.Stage = 3
-			if conversation.Type == 1 {
+			if ctx.Conversation.Edited {
+				ctx.Conversation.Stage = 6
+				ctx.Conversation.Edited = false
+			} else {
+				ctx.Conversation.Stage = 3
+			}
+			if ctx.Conversation.Type == 1 {
 				err = replyTextWithCtx(ctx, askLostItemPrompt)
 			} else {
 				err = replyTextWithCtx(ctx, askFoundItemPrompt)
@@ -182,18 +181,22 @@ func stage2AddConversation(ctx conversation.ConversationContext) (err error) {
 // 阶段3 添加物品名称 需要进行确认
 func stage3ItemConversation(ctx conversation.ConversationContext) (err error) {
 	content := ctx.ReceiveContent.Content
-	conversation := conversationMap[ctx.ReceiveContent.FromUsername]
-	switch conversation.Status {
+	switch ctx.Conversation.Status {
 	case "":
-		conversation.Form.ItemName = content
+		ctx.Conversation.Form.ItemName = content
 		err = replyTextWithCtx(ctx, fmt.Sprintf("物品名称为:%s\n1.yes\n2.no", content))
-		conversation.Status = "waitconfirm"
+		ctx.Conversation.Status = "waitconfirm"
 	case "waitconfirm":
-		conversation.Status = ""
+		ctx.Conversation.Status = ""
 		switch content {
 		case "1", "yes":
-			conversation.Stage = 4
-			if conversation.Type == 1 {
+			if ctx.Conversation.Edited {
+				ctx.Conversation.Stage = 6
+				ctx.Conversation.Edited = false
+			} else {
+				ctx.Conversation.Stage = 4
+			}
+			if ctx.Conversation.Type == 1 {
 				err = replyTextWithCtx(ctx, askLostDescriptionPrompt)
 			} else {
 				err = replyTextWithCtx(ctx, askPickDescriptionPrompt)
@@ -211,24 +214,28 @@ func stage3ItemConversation(ctx conversation.ConversationContext) (err error) {
 // 阶段4 添加描述 需要确认 	并生成标签,~~询问用户是否还要加上新的标签~~（暂时不做...）
 func stage4DescriptionConversation(ctx conversation.ConversationContext) (err error) {
 	content := ctx.ReceiveContent.Content
-	conversation := conversationMap[ctx.ReceiveContent.FromUsername]
-	switch conversation.Status {
+	switch ctx.Conversation.Status {
 	case "":
-		conversation.Status = "waitconfirm"
-		conversation.Form.Description = content
+		ctx.Conversation.Status = "waitconfirm"
+		ctx.Conversation.Form.Description = content
 		err = replyTextWithCtx(ctx, fmt.Sprintf("您的描述是:\n%s\n1.yes\n2.no", content))
 	case "waitconfirm":
-		conversation.Status = ""
+		ctx.Conversation.Status = ""
 		switch content {
 		case "1", "yes":
-			conversation.Stage = 5
+			if ctx.Conversation.Edited {
+				ctx.Conversation.Stage = 6
+				ctx.Conversation.Edited = false
+			} else {
+				ctx.Conversation.Stage = 5
+			}
 			allTextBuilder := strings.Builder{}
-			allTextBuilder.WriteString(conversation.Form.City)
-			allTextBuilder.WriteString(conversation.Form.ItemName)
-			allTextBuilder.WriteString(conversation.Form.Description)
+			allTextBuilder.WriteString(ctx.Conversation.Form.City)
+			allTextBuilder.WriteString(ctx.Conversation.Form.ItemName)
+			allTextBuilder.WriteString(ctx.Conversation.Form.Description)
 			tags := GenerateTags(allTextBuilder.String())
 			log.Println("物品TAGS:", tags)
-			conversation.Form.ItemTags = tags
+			ctx.Conversation.Form.ItemTags = tags
 			err = replyTextWithCtx(ctx, askImgPrompt)
 		case "2", "no":
 			fallthrough
@@ -314,7 +321,7 @@ func askForConfirm(ctx conversation.ConversationContext) (err error) {
 				ctx.Conversation.Stage = 6
 				err = replyTextWithCtx(ctx, "取消选择，输入任意文本继续操作")
 			case "7":
-				err = replyTextWithCtx(ctx, "已删除会话")
+				err = replyTextWithCtx(ctx, "已取消该次会话")
 				delete(conversationMap, ctx.ReceiveContent.FromUsername)
 			}
 		}
@@ -324,7 +331,7 @@ func askForConfirm(ctx conversation.ConversationContext) (err error) {
 		case "1", "yes":
 			// 提交至数据库
 			err = dao.AddRecord(ctx)
-			replyTextWithCtx(ctx, "已添加记录")
+			err = replyTextWithCtx(ctx, "已添加记录")
 		case "2", "no":
 			fallthrough
 		default:
